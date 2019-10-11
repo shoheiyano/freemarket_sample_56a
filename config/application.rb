8bc51b50ee6da7be22543400a5aa4f575ce72973
shoheiyano <yspersona@yahoo.co.jp>
2019-10-11 16:37:57 +0900

    不要なファイルを削除

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,10 @@ module FreemarketSample56a
     config.load_defaults 5.2
     config.generators do |g|
       g.javascripts false
+      g.stylesheets false
       g.helper false
       g.test_framework false
+        
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
