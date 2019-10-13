113a55f3625c84c75e07f4de4a38dcfeb700683f
kiji0815 <nikorasu0125@gmail.com>
2019-10-13 16:47:17 +0900

    自動デプロイのエラーの細かな修正

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,11 +7,11 @@ worker_processes 1
 #アプリケーションの設置されているディレクトリを指定
 working_directory "#{app_path}/current"
 
-#Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-
 #ポート番号を指定
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+
+#Unicornの起動に必要なファイルの設置場所を指定
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
@@ -21,8 +21,6 @@ stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
-
-#以下は応用的な設定なので説明は割愛
 
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
