697fb9cb486bac8150304bc06764beec5af2d462
kiji0815 <nikorasu0125@gmail.com>
2019-10-17 00:01:05 +0900

    全角空白の修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.2'
 set :application, 'freemarket_sample_56a'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:kiji0815/freemarket_sample_56a.git'
+set :repo_url, 'git@github.com:kiji0815/freemarket_sample_56a.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
