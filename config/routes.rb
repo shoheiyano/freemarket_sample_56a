1d576bfe48de4a75e6571ff87b0707d5d40c2f65
tmysmmr <motha.815@gmail.com>
2019-10-20 21:36:50 +0900

    本人情報の登録(identification) 現状プルリク

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'items#index'
   devise_for :users
+  get 'mypage/identification', action: :edit, controller: 'identification'
 end
