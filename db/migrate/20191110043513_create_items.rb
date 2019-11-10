1ed8a4b433f8202604438b17c25fab729cb6492d
kiji0815 <nikorasu0125@gmail.com>
2019-11-10 15:20:07 +0900

    アイテムテーブルの作成

--- db/migrate/20191110043513_create_items.rb
+++ db/migrate/20191110043513_create_items.rb
@@ -1,6 +1,17 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
+      t.string            :trade_name, null: false
+      t.text              :description, null: false
+      t.string            :condition, null: false
+      t.string            :postage, null: false
+      t.string            :delivery_method, null: false
+      t.string            :shipment_area, null: false
+      t.string            :shipment_date, null: false
+      t.integer           :price, null: false
+      t.references        :seller_id, null: false, foreign_key: true
+      t.references        :buyer_id
+      t.references        :user_id, null: false, foreign_key: true
 
       t.timestamps
     end
