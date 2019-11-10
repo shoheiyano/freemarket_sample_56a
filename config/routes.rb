5aa337d87abbd9ea10a3017f4c52cee1efdc1d54
shoheiyano <yspersona@yahoo.co.jp>
2019-11-10 19:55:00 +0900

    商品詳細ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   root to: 'items#index'
   devise_for :users
   get 'mypage/identification', action: :edit, controller: 'identification'
   get 'mypage/profile', action: :edit, controller: 'profile'
   get "logout" => "profile#logout"
   get 'mypage' => 'profile#mypage'
+  get 'items/:id' => 'items#show' 
 
   #ユーザー各種新規登録画面
   devise_scope :user do
