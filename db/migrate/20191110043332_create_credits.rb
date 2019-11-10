28c38abd7bf214f6b5199a97b547785120f80ad0
kiji0815 <nikorasu0125@gmail.com>
2019-11-10 15:06:46 +0900

    クレジットテーブルの作成

--- db/migrate/20191110043332_create_credits.rb
+++ db/migrate/20191110043332_create_credits.rb
@@ -1,6 +1,11 @@
 class CreateCredits < ActiveRecord::Migration[5.2]
   def change
     create_table :credits do |t|
+      t.integer       :card_number, null: false
+      t.integer       :expiration_month, null: false
+      t.integer       :expiration_year, null: false
+      t.integer       :security_cord, null: false
+      t.references    :user_id, null: false, foreign_key: true
 
       t.timestamps
     end
