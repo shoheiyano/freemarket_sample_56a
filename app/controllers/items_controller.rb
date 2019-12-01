5dd5aedf515df0231918e2c4cd991256bcc181cb
tmysmmr <motha.815@gmail.com>
2019-12-01 14:02:43 +0900

    商品出品ページ　一旦マージ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,48 @@
 class ItemsController < ApplicationController
+
   def index
   end
 
   def new
+    @item = Item.new
+    @parents = Category.where(ancestry: nil).order("id ASC").limit(13)
+    @item.size
+    @item.brand
+    @item.photos
   end
-  
+
+  def search
+    respond_to do |format|
+      format.html
+      format.json do
+        @children = Category.find(params[:parent_id]).children
+         # 親カテゴリーのidから子カテゴリーのidの配列を作成してインスタンス変数で定義
+      end
+    end
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save!
+      size_id = Size.find(@item.id).id
+      brand_id = Brand.find(@item.id).id
+      item = Item.find(@item.id)
+      item.update(size_id: size_id)
+      item.update(brand_id: brand_id)
+      redirect_to root_path
+    else
+      redirect_to new_item_path
+    end
+  end
 
   def show
     
   end
 
+  private
 
-  
-    private
+  def item_params
+    params.require(:item).permit(:name, :description, :condition, :postage, :delivery_method, :prefecture_id, :shipment_date, :price, size_attributes: [:id, :size], brand_attributes: [:id, :name], photo_attributes: [:id, :url], category_ids: []).merge(seller_id: current_user.id)
+  end
 
-    def item_params
-      params.require(:item).permit(:name)
-    end
 end
