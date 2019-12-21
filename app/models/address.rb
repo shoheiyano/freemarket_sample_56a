360f2354eb360855de07c79428a90f95848926c8
kiji0815 <nikorasu0125@gmail.com>
2019-12-21 17:15:47 +0900

    presence: trueをまとめました

--- app/models/address.rb
+++ app/models/address.rb
@@ -7,14 +7,10 @@ class Address < ApplicationRecord
   #新規登録のバリデーション
   VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/ #電話番号ハイフンなし10桁か11桁
   # #address
-  validates :last_name,       presence: true
-  validates :first_name,      presence: true
-  validates :last_name_kana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/, allow_blank: true } #カタカナ
-  validates :first_name_kana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/, allow_blank: true } #カタカナ
-  validates :postal_cord, presence: true, format:{ with: /\A\d{3}[-]\d{4}\z/, allow_blank: true } #ハイフンあり7桁
-  validates :prefecture_id,   presence: true
-  validates :city,            presence: true
-  validates :block,           presence: true
+  validates :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_cord, :prefecture_id, :city, :block, presence: true
+  validates :last_name_kana, format: { with: /\A([ァ-ン]|ー)+\z/, allow_blank: true } #カタカナ
+  validates :first_name_kana, format: { with: /\A([ァ-ン]|ー)+\z/, allow_blank: true } #カタカナ
+  validates :postal_cord, format:{ with: /\A\d{3}[-]\d{4}\z/, allow_blank: true } #ハイフンあり7桁
   validates :phone_number, format:{ with: VALID_PHONE_REGEX, allow_blank: true }
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,19 +19,15 @@ class User < ApplicationRecord
   #新規登録画面バリデーション
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   #registration
-  validates :nickname, presence: true
-  validates :email, presence: true, format:{ with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :password, length:{minimum:7}, presence: true
-  validates :password_confirmation, length:{minimum:7}, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/, allow_blank: true } #カタカナ
-  validates :first_name_kana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/, allow_blank: true } #カタカナ
-  validates :birth_year, presence: true
-  validates :birth_month, presence: true
-  validates :birth_day, presence: true
+  validates :nickname, :email, :password, :password_confirmation, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_year, :birth_month, :birth_day, :phone_number, presence: true
+  validates :email, format:{ with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, length:{minimum:7}
+  validates :password_confirmation, length:{minimum:7}
+  validates :last_name_kana, format: { with: /\A([ァ-ン]|ー)+\z/, allow_blank: true } #カタカナ
+  validates :first_name_kana, format: { with: /\A([ァ-ン]|ー)+\z/, allow_blank: true } #カタカナ
+
   # #sms_confirmation
-  validates :phone_number, presence: true, format: { with: /\A\d{11}\z/, allow_blank: true } #ハイフンなし11桁
+  validates :phone_number, format: { with: /\A\d{11}\z/, allow_blank: true } #ハイフンなし11桁
 
 
   #SNS認証のために記入
