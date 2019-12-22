b451c7f1982a298cfc341e90ffef57db986d8bb8
shoheiyano <yspersona@yahoo.co.jp>
2019-12-22 20:29:17 +0900

    商品詳細ページにDB情報を

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,7 @@ class ItemsController < ApplicationController
     @parents = Category.where(ancestry: nil).order("id ASC").limit(13)
     # @item.size
     # @item.brand
-    @item.photos.new #_buildの書き方でエラーが出ました。コネクトで聞いた結果、同じ意味である左記の記述で書いてあります。
+    @item_photo = @item.photos.build #_buildの書き方でエラーが出ました。コネクトで聞いた結果、同じ意味である左記の記述で書いてあります。
   end
 
   def search
@@ -27,7 +27,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    # binding.pry
+    binding.pry
     @item = Item.new(item_params)
       if @item.save
         redirect_to root_path
@@ -44,6 +44,7 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @items = Item.find(params[:id])
   end
 
   private
