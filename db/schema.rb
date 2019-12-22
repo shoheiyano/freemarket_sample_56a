5b965620f30290be82ccf96d5198b23abbaee8fa
kiji0815 <nikorasu0125@gmail.com>
2019-12-22 18:35:47 +0900

    デプロイのため一度マージしたいと思います

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_20_074249) do
+ActiveRecord::Schema.define(version: 2019_12_21_072324) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
