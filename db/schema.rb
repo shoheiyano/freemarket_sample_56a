b7b72815e83dba7358ff79921d6ebc4d539718ed
tmysmmr <motha.815@gmail.com>
2019-12-24 22:33:22 +0900

    商品詳細　細かな修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_21_072324) do
+ActiveRecord::Schema.define(version: 2019_12_24_131421) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "prefecture_id"
@@ -74,7 +74,7 @@ ActiveRecord::Schema.define(version: 2019_12_21_072324) do
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "trade_name", null: false
     t.text "description", null: false
-    t.integer "condition", null: false
+    t.string "condition", null: false
     t.string "postage", null: false
     t.string "delivery_method", null: false
     t.string "shipment_area", null: false
