582a790aecd9fbb31ba79280171a4c25c8320846
kiji0815 <nikorasu0125@gmail.com>
2019-12-28 12:25:54 +0900

    デプロイのためマージします

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -28,103 +28,38 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   #   super(scope)
   # end
 
-  def facebook
-    @user = User.from_omniauth(request.env["omniauth.auth"])
 
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to registration_signup_index_path
-    end
+  def facebook
+    callback_for(:facebook)
   end
 
   def google_oauth2
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    callback_for(:google)
+  end
 
-    if @user.persisted?
+  #userモデルでsnsから取得した情報を元にデータベースでユーザーを探し、なければ作る
+  def callback_for(provider)
+    @omniauth = request.env['omniauth.auth'] #userモデルでsnsから取得したnickname,email,uid,provider,tokenなどが入っている
+    info = User.find_oauth(@omniauth) #infoの中身は、@omniauthの中からuserテーブル、sns_credentialsテーブルに保存する分が入っている。:user,:snsの形なのでinfo[:user]ならuserテーブルに入れる情報が取れる
+    @user = info[:user] #@omniauthからuserテーブルに入れる分が入っている。nicknameとemailが入っている。
+    binding.pry
+    if @user.persisted? #@userがsave済みであるかの確認
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'google') if is_navigational_format?
-    else
-      session["devise.google_data"] = request.env["omniauth.auth"][:info]
-      redirect_to registration_signup_index_path
+      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+      binding.pry
+    else #@userがsave済みじゃなければこっちにくる
+      @sns = info[:sns] #@omniauthのうち、sns_credentialsテーブルに入れる分を取って@snsに渡す。uid,provider。
+      # binding.pry
+      render template: "signup/registration" #@user,@snsの情報を持って新規会員登録ページにとぶ。nickname,email,uid,providerを持ってる。
+      binding.pry #passwordが発行されても新規登録画面に出てこない
     end
   end
 
-#   def facebook
-#     callback_for(:facebook)
-#   end
+  def failure
+    redirect_to root_path and return
+  end
 
-#   def google_oauth2
-#     callback_for(:google)
-#   end
-
-#   # common callback method
-#   def callback_for(provider)
-#     info = User.find_oauth(request.env["omniauth.auth"])
-#     # snsの情報からuserが登録されているか or snsから情報を取得できているかを確認
-#     @user = User.where(nickname: info[:user][:nickname]).or(User.where(email: info[:user][:email])).first || info[:user]
-
-#     # persisted?はデータがDBに保存されているかを確認する/配列に対しては使えないから@userを定義するときは気をつける
-#     if @user.persisted?
-#       #保存されていればログインしてroot_pathにリダイレクトされる
-#       sign_in_and_redirect @user, event: :authentication
-#       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
-#     else
-#       # 登録するアクションに取得した値を渡すために。sessionを利用してuserインスタンスを作成する
-#       session[:nickname] = info[:user][:nickname]
-#       session[:email] = info[:user][:email]
-
-#       #snsでのユーザ登録ではパスワードを入力させないので準備する。パスワードを作成するDeviseメソッドもある。
-#       #今回のバリデーションは英数字のみなのでこっちを採用
-#       session[:password_confirmation] = SecureRandom.alphanumeric(30)
-
-#       #SnsCredentialが登録されていないとき
-#       if SnsCredential.find_by(uid: info[:sns][:uid], provider: info[:sns][:provider]).nil?
-#         #ユーザ登録と同時にsns_credentialも登録するために
-#         session[:uid] = info[:sns][:uid]
-#         session[:provider] = info[:sns][:provider]
-#       end
-#       #登録フォームのviewにリダイレクトさせる
-#       redirect_to registration_signups_path
-#     end
-#   end
-
-#   def failure
-#     redirect_to root_path
-#   end
-# end
-
-  # def facebook
-  #   callback_for(:facebook)
-  #   # binding.pry #4番目に動いた
-  # end
-
-  # def google_oauth2
-  #   callback_for(:google)
-  # end
-
-
-  # def callback_for(provider)
-  #   @omniauth = request.env['omniauth.auth']
-  #   info = User.find_oauth(@omniauth) #infoにはuser(emailとnickname)、sns(providerとuid)が入っている
-  #   @user = info[:user] #userモデルに入れるemailとnicknameを取得している
-  #   # binding.pry #3番目に動いた
-  #   if @user.persisted? 
-  #     # binding.pry
-  #     sign_in_and_redirect @user, event: :authentication
-  #     set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
-  #     # binding.pry
-  #   else 
-  #     @sns = info[:sns]
-  #     render "signup/registration/"
-  #     # binding.pry
-  #   end
-  # end
-
-  # def failure
-  #   redirect_to root_path and return
-  # end
 end
+
+
 
