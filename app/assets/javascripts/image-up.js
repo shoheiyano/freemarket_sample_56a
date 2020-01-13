861018e3483a72adb0fa7f947c76740700631250
shoheiyano <yspersona@yahoo.co.jp>
2020-01-13 16:10:08 +0900

    データを取得しinput

--- app/assets/javascripts/image-up.js
+++ app/assets/javascripts/image-up.js
@@ -1,4 +1,8 @@
 $(function(){
+
+  //取得したカスタムデータ属性がhtmlに引数として渡され、下の場所に表示される
+  var input_place = $(".header-signup");
+
   //DataTransferオブジェクトで、データを格納する箱を作る
   var dataBox = new DataTransfer();
   //querySelectorでfile_fieldを取得
@@ -46,37 +50,58 @@ $(function(){
   }));
   //削除ボタンをクリックすると発火するイベント
   //削除ボタンをクリックすると発火するイベント
-$(document).on("click", '.item-image__operetion--delete', function(){
-  //削除を押されたプレビュー要素を取得
-  var target_image = $(this).parent().parent()
-  //削除を押されたプレビューimageのfile名を取得
-  var target_name = $(target_image).data('image')
-  //プレビューがひとつだけの場合、file_fieldをクリア
-  if(file_field.files.length==1){
-    //inputタグに入ったファイルを削除
-    $('input[type=file]').val(null)
-    dataBox.clearData();
-    console.log(dataBox)
-  }else{
-    //プレビューが複数の場合
-    $.each(file_field.files, function(i,input){
-      //削除を押された要素と一致した時、index番号に基づいてdataBoxに格納された要素を削除する
-      if(input.name==target_name){
-        dataBox.items.remove(i) 
+
+
+  $(document).on("click", '.item-image__operetion--delete', function(){
+
+    
+        
+        //データ属性を取得してinputに送る
+      function appendImages(image_id) {
+        var html =`<div class="input-area">
+                  <input multiple="multiple" type="file" class="sell__upload__drop-file" id="post_img_last" name="item[images][] value=${ image_id }">
+                  <label class="text" for="post_img_last">
+                  </label>
+                  </div>`
+
+      input_place.append(html);
       }
-    })
-    //DataTransferオブジェクトに入ったfile一覧をfile_fieldの中に再度代入
-    file_field.files = dataBox.files
-  }
-  //プレビューを削除
-  target_image.remove()
+
+      var image_id = $(this).parent().parent().data("image-id");
+          console.log(image_id);
+
+              $(this).parent().parent().remove();
+              appendImages(image_id);
+
+  //削除を押されたプレビュー要素を取得
+  //////////////////////////////////////////////////////////一旦隠す。いらない？と思い隠した記述
+  // var target_image = $(this).parent().parent()
+  // //削除を押されたプレビューimageのfile名を取得
+  // var target_name = $(target_image).data('image')
+  // //プレビューがひとつだけの場合、file_fieldをクリア
+  // if(file_field.files.length==1){
+  //   //inputタグに入ったファイルを削除
+  //   $('input[type=file]').val(null)
+  //   dataBox.clearData();
+  //   console.log(dataBox)
+  // }else{
+  //   //プレビューが複数の場合
+  //   $.each(file_field.files, function(i,input){
+  //     //削除を押された要素と一致した時、index番号に基づいてdataBoxに格納された要素を削除する
+  //     if(input.name==target_name){
+  //       dataBox.items.remove(i) 
+  //     }
+  //   })
+  //   //DataTransferオブジェクトに入ったfile一覧をfile_fieldの中に再度代入
+  //   file_field.files = dataBox.files
+  // }
+  // //プレビューを削除
+  // target_image.remove()
   //image-box__containerクラスをもつdivタグのクラスを削除のたびに変更
   var num = $('.item-image').length
   $('#image-box__container').show()
   $('#image-box__container').attr('class', `item-num-${num}`)
 })
-
-
 
 
 });
