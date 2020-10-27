f73fef56173d9f590d1eb4c125ccda76b7a460c4
shoheiyano <yspersona@yahoo.co.jp>
2020-10-27 13:03:03 +0900

    change

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.11.2'
 set :application, 'freemarket_sample_56a'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url, 'git@github.com:kiji0815/freemarket_sample_56a.git'
+set :repo_url, 'git@github.com:shoheiyano/freemarket.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
@@ -18,7 +18,7 @@ set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/merukari.pem']
+                  keys: ['~/.ssh/freemarket.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
